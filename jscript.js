7f27f7af723367d37e654c881f2a9bed5f6e5267
lokeshloki3 <lokesh1992p@gmail.com>
2024-07-11 22:52:27 +0530

    html file index.html

---
+++ jscript.js
@@ -0,0 +1,21 @@
+const countValue = document.querySelector('#counter');
+// const countValue = document.getElementById('counter');
+
+function increment(){
+    // get the value from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
+
+
+function decrement(){
+        // get the value from UI
+        let value = parseInt(countValue.innerText);
+        //Update the value
+        value = value - 1;
+        //set the value onto UI
+        countValue.innerText = value;
+};
